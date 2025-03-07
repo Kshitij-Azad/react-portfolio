e4e549339d7e33e3bf09fb74b34b2f4d04a38a1b
Kshitij-Azad <kshitijazad.ka@gmail.com>
2025-03-08 01:31:05 +0530

    Update Header and add more Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const handleMouseEnter = () => setIsOpen(true);
+  const handleMouseLeave = () => setIsOpen(false);
+
 
   return (
     <nav className="bg-cyan-950 p-4 text-white">
@@ -9,30 +12,30 @@ export const Header = () => {
         <a href="#" className="text-lg font-bold">Kshitij Azad</a>
 
         <button
-          onClick={() => setIsOpen(!isOpen)}
+          onMouseEnter={handleMouseEnter}
           className="md:hidden text-2xl"
         >
           ☰
         </button>
 
         <ul className="hidden md:flex space-x-6">
-          <li><a href="#" className="hover:text-blue-600 ">Home</a></li>
-          <li><a href="#" className="hover:text-gray-700">About</a></li>
-          <li><a href="#" className="hover:text-gray-700">Skills</a></li>
-          <li><a href="#" className="hover:text-gray-700">Projects</a></li>
-          <li><a href="#" className="hover:text-gray-700">Experience</a></li>
-          <li><a href="#" className="hover:text-gray-700">Contact</a></li>
+          <li><a href="#" className="hover:text-yellow-300 ">Home</a></li>
+          <li><a href="#" className="hover:text-yellow-300">About</a></li>
+          <li><a href="#" className="hover:text-yellow-300">Skills</a></li>
+          <li><a href="#" className="hover:text-yellow-300">Projects</a></li>
+          <li><a href="#" className="hover:text-yellow-300">Experience</a></li>
+          <li><a href="#" className="hover:text-yellow-300">Contact</a></li>
         </ul>
       </div>
 
       {isOpen && (
-        <ul className="flex flex-col space-y-4 bg-cyan-950 p-4 text-white mt-2 md:hidden" id="toggle">
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">Home</a></li>
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">About</a></li>
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">Skills</a></li>
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">Projects</a></li>
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">Experience</a></li>
-          <li><a href="#" className="border py-1 px-1 ml-1 block hover:text-gray-700">Contact</a></li>
+        <ul className="flex flex-col space-y-4 bg-cyan-950 p-4 text-white mt-2 md:hidden" id="toggle" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+          <li><a href="#" className="block hover:text-yellow-300">Home</a></li>
+          <li><a href="#" className="block hover:text-yellow-300">About</a></li>
+          <li><a href="#" className="block hover:text-yellow-300">Skills</a></li>
+          <li><a href="#" className="block hover:text-yellow-300">Projects</a></li>
+          <li><a href="#" className="block hover:text-yellow-300">Experience</a></li>
+          <li><a href="#" className="block hover:text-yellow-300">Contact</a></li>
         </ul>
       )}
     </nav>
